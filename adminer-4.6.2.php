66f4bf8cd39efc590a51e0d35b95336490e591a0
gun1234 <39216487+gun1234@users.noreply.github.com>
2018-05-12 19:33:14 +0700

    testing

--- adminer-4.6.2.php
+++ adminer-4.6.2.php
@@ -6,6 +6,7 @@
 * @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
 * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
 * @version 4.6.2
+//testing by gunawan
 */error_reporting(6135);$Vc=!preg_match('~^(unsafe_raw)?$~',ini_get("filter.default"));if($Vc||ini_get("filter.default_flags")){foreach(array('_GET','_POST','_COOKIE','_SERVER')as$X){$_i=filter_input_array(constant("INPUT$X"),FILTER_UNSAFE_RAW);if($_i)$$X=$_i;}}if(function_exists("mb_internal_encoding"))mb_internal_encoding("8bit");function
 connection(){global$g;return$g;}function
 adminer(){global$b;return$b;}function
